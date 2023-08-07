aee9d4e18dc891559cc6f0ba145d1783c25ff946
elias782K <141656518+elias782K@users.noreply.github.com>
2023-08-07 18:20:30 +0200

    Update 1+ money every click Script.lua

--- 1+ money every click Script.lua
+++ 1+ money every click Script.lua
@@ -185,3 +185,14 @@ end
         elias7 = 1246766454
     end
 end)
+
+
+
+mainsec:NewButton("AntiAfk", "Does So you dont get kicked from being afk", function()
+    local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+wait(1)
+vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+end)
